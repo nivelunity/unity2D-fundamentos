99739c42caefb32a670f28177b343f54dcb39b5f
Gaston Caminiti <gastoncaminiti@gmail.com>
2023-08-09 14:29:34 -0300

    Trampolin Prefab

--- Proyecto Unity 2D/Assets/Scripts/Jugador.cs
+++ Proyecto Unity 2D/Assets/Scripts/Jugador.cs
@@ -10,5 +10,19 @@ public class Jugador : MonoBehaviour
     public void ModificarVida(float puntos)
     {
         vida += puntos;
+        Debug.Log(EstasVivo());
+    }
+
+
+    private bool EstasVivo()
+    {
+        return vida > 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Meta")) { return; }
+
+        Debug.Log("GANASTE");
     }
 }
