1956f1e773ec753e0ef44ae70f2ec62bf90b856a
Gaston Caminiti <gastoncaminiti@gmail.com>
2023-08-23 11:52:06 -0300

    Saltar Mask Layer

--- Proyecto Unity 2D/Assets/Scripts/Mover.cs
+++ Proyecto Unity 2D/Assets/Scripts/Mover.cs
@@ -16,6 +16,9 @@ public class Mover : MonoBehaviour
     private Rigidbody2D miRigidbody2D;
     private Animator miAnimator;
     private SpriteRenderer miSprite;
+    private CircleCollider2D miCollider2D;
+
+    private int saltarMask;
 
     // Codigo ejecutado cuando el objeto se activa en el nivel
     private void OnEnable()
@@ -23,6 +26,8 @@ public class Mover : MonoBehaviour
         miRigidbody2D = GetComponent<Rigidbody2D>();
         miAnimator = GetComponent<Animator>();
         miSprite = GetComponent<SpriteRenderer>();
+        miCollider2D = GetComponent<CircleCollider2D>();
+        saltarMask = LayerMask.GetMask("Pisos", "Plataformas");
     }
 
     // Codigo ejecutado en cada frame del juego (Intervalo variable)
@@ -30,16 +35,22 @@ public class Mover : MonoBehaviour
     {
         moverHorizontal = Input.GetAxis("Horizontal");
         direccion = new Vector2(moverHorizontal, 0f);
-        
+       
         int velocidadX = (int)miRigidbody2D.velocity.x;
         miSprite.flipX = velocidadX > 0;
         miAnimator.SetInteger("Velocidad", velocidadX);
 
-        miAnimator.SetBool("EnAire", miRigidbody2D.velocity.y != 0f);
+        miAnimator.SetBool("EnAire", !EnContactoConPlataforma());
+        
     }
 
     private void FixedUpdate()
     {
         miRigidbody2D.AddForce(direccion * velocidad);
+    }
+
+    private bool EnContactoConPlataforma()
+    {
+        return miCollider2D.IsTouchingLayers(saltarMask);
     }
 }
