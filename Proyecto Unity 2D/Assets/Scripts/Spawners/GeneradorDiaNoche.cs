9b8a06200e27b0de70dbbc8147db90554ffc6c87
Gaston Caminiti <gastoncaminiti@gmail.com>
2023-09-12 10:51:55 -0300

    Refactor Corrutina Dia y Noche

--- Proyecto Unity 2D/Assets/Scripts/Spawners/GeneradorDiaNoche.cs
+++ Proyecto Unity 2D/Assets/Scripts/Spawners/GeneradorDiaNoche.cs
@@ -6,24 +6,32 @@ public class GeneradorDiaNoche : MonoBehaviour
 {
     [SerializeField] private Camera camara;
     [SerializeField] private Color nocheColor;
-    [SerializeField] private float segundos;
+
+
+    [SerializeField][Range(4,128)] private int duracionDia;
+    [SerializeField][Range(4, 24)] private int dias;
 
     private Color diaColor;
 
     void Start()
     {
         diaColor = camara.backgroundColor;
-        StartCoroutine(CambiarColor(segundos)); ;
+        StartCoroutine(CambiarColor(duracionDia));
     }
 
     IEnumerator CambiarColor(float tiempo)
     {
-        while (true)
+        for (int i = 0; i < duracionDia; i++)
         {
             yield return new WaitForSeconds(tiempo);
-            camara.backgroundColor = camara.backgroundColor == diaColor ?
-                                        camara.backgroundColor = nocheColor :
-                                        camara.backgroundColor = diaColor;
+            InvertirColor();
         }
+    }
+
+    private void InvertirColor()
+    {
+        camara.backgroundColor = camara.backgroundColor == diaColor ?
+                                    camara.backgroundColor = nocheColor :
+                                    camara.backgroundColor = diaColor;
     }
 }
